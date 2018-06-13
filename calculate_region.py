371e171faf9046b0c52cc4bb9e5859869c789aec
홍진석 <home0085@naver.com>
2018-06-13 22:29:11 +0900

    FP상황 없애는 코드 구현

--- calculate_region.py
+++ calculate_region.py
@@ -8,9 +8,55 @@ class CalculateRegion(object):
 
 
     def __init__(self):
+        self.cnt = 0
+        self.cnt1 = 0
+        self.Warning = False
         pass
 
-    def calculate_region(self, image, bounding_box, lines):
+    def calculate_region_image(self,image, bounding_box, lines):
+
+        leftx1, lefty1, leftx2, lefty2 = lines[0]  # 왼쪽 선 좌표
+        rightx1, righty1, rightx2, righty2 = lines[1]  # 오른쪽 선 좌표
+
+        for box in bounding_box:
+            left, right, top, bottom = box  # bounding box 좌표
+            bol_area = self.calculate_boundingbox(box)
+            if bol_area == True:
+                find_left = self.calculate_leftline(leftx1, lefty1, leftx2, lefty2, box)  # 왼쪽 선에 겹치는지 확인
+                find_right = self.calculate_rightline(rightx1, righty1, rightx2, righty2, box)  # 오른쪽 선에 겹치는지 확인
+                print("왼쪽선 중첩 ? : " + str(find_left) + ', 오른쪽선 중첩? : ' + str(find_right))
+                if find_left == True:  # 왼쪽 선에 겹치면 도형 판단하여 도형 넓이 계산
+                    polygon = self.decide_leftpolygon(leftx1, lefty1, leftx2, lefty2, box)
+                    print("Polygon(triangle - 1, rectangle - 2, pentagon - 3) : " + str(polygon))
+                    if polygon == 1:
+                        result = self.calculate_lefttriangle(leftx1, lefty1, leftx2, lefty2, box)
+                    elif polygon == 2:
+                        result = self.calculate_leftrectangle(leftx1, lefty1, leftx2, lefty2, box)
+                    elif polygon == 3:
+                        result = self.calculate_leftpentagon(leftx1, lefty1, leftx2, lefty2, box)
+                elif find_right == True:  # 오른쪽 선에 겹치면 도형 판단하여 도형 넓이 계산
+                    polygon = self.decide_rightpolygon(rightx1, righty1, rightx2, righty2, box)
+                    if polygon == 1:
+                        result = self.calculate_righttriangle(rightx1, righty1, rightx2, righty2, box)
+                    elif polygon == 2:
+                        result = self.calculate_rightrectangle(rightx1, righty1, rightx2, righty2, box)
+                    elif polygon == 3:
+                        result = self.calculate_rightpentagon(rightx1, righty1, rightx2, righty2, box)
+                else:  # 둘 다 아니면 다음 box 검사
+                    continue
+                rectangle = abs((right - left)) * abs((bottom - top))  # box 사각형 넓이
+                percentage = result / rectangle * 100  # box 대비 중첩된 영역 비율
+                print("중첩 영역의 넓이 : " + str(result))
+                print("Bounding box의 넓이 : " + str(rectangle))
+                print("중첩 비율 : " + str(percentage))
+                if percentage >= 20 and percentage < 60:
+                    image = self.change_image(image)
+                    break
+            else:
+                continue
+        return image
+
+    def calculate_region_video(self, image, bounding_box, lines):
         '''
         겹치는 영역 판단하여 겹치는 부분있으면 퍼센테이지 확인하여 위험 탐지 알림
         :param bounding_box: box list
@@ -22,15 +68,14 @@ class CalculateRegion(object):
 
         for box in bounding_box:
             left, right, top, bottom = box  # bounding box 좌표
-
             bol_area = self.calculate_boundingbox(box)
-
             if bol_area == True:
                 find_left = self.calculate_leftline(leftx1, lefty1, leftx2, lefty2, box)    # 왼쪽 선에 겹치는지 확인
                 find_right = self.calculate_rightline(rightx1, righty1, rightx2, righty2, box)  # 오른쪽 선에 겹치는지 확인
-                print(find_left, find_right)
+                print("왼쪽선 중첩 ? : " + str(find_left) + ', 오른쪽선 중첩? : ' + str(find_right))
                 if find_left == True:   # 왼쪽 선에 겹치면 도형 판단하여 도형 넓이 계산
                     polygon = self.decide_leftpolygon(leftx1, lefty1, leftx2, lefty2, box)
+                    print("Polygon(triangle - 1, rectangle - 2, pentagon - 3) : " + str(polygon))
                     if polygon == 1:
                         result = self.calculate_lefttriangle(leftx1, lefty1, leftx2, lefty2, box)
                     elif polygon == 2:
@@ -49,10 +94,22 @@ class CalculateRegion(object):
                     continue
                 rectangle = abs((right-left)) * abs((bottom-top))   # box 사각형 넓이
                 percentage = result / rectangle * 100               # box 대비 중첩된 영역 비율
-                print(percentage)
-                if percentage >= 20 and percentage < 50:
-                    image = self.change_image(image)
-                    break
+                print("중첩 영역의 넓이 : " + str(result))
+                print("Bounding box의 넓이 : " + str(rectangle))
+                print("중첩 비율 : " + str(percentage))
+                # WARNING 조건문
+                if percentage >= 20 and percentage < 60:
+                    self.cnt += 1
+                else:
+                    if self.cnt >= 15:
+                        self.cnt = 0
+                        self.Warning = True
+                        image = self.change_video(image)
+                        break
+                    else:
+                        self.cnt = 0
+                if self.Warning == True:
+                    image = self.change_video(image)
             else:
                 continue
         return image
@@ -60,6 +117,7 @@ class CalculateRegion(object):
     def calculate_boundingbox(self , bounding_box):
         left, right, top, bottom = bounding_box
         area = (right - left) * (bottom - top)
+        print(str(bounding_box) + ", Area : "+str(area))
         if area > 30000 :
             return True
         else:
@@ -150,7 +208,7 @@ class CalculateRegion(object):
         c = [((bottom - (y1 - ((y2 - y1) / (x2 - x1) * x1))) / ((y2 - y1) / (x2 - x1))), bottom]
         ac = math.sqrt(((c[1]-a[1])*(c[1]-a[1])) + ((c[0]-a[0])*(c[0]-a[0])))
         ab = math.sqrt(((b[1]-a[1])*(b[1]-a[1])) + ((b[0]-a[0])*(b[0]-a[0])))
-        return (ac * ab / 2)
+        return abs(ac * ab / 2)
 
     def calculate_leftrectangle(self, x1, y1, x2, y2, box):
 
@@ -161,7 +219,7 @@ class CalculateRegion(object):
         top_width = right - line_top
         height = bottom - top
         area = (bottom_width + top_width) * height / 2
-        return area
+        return abs(area)
 
 
     def calculate_leftpentagon(self, x1, y1, x2, y2, box):
@@ -172,7 +230,7 @@ class CalculateRegion(object):
         rec_area = (bottom - top) * (right-left)
         tri_area = (line_top - left) * (line_bottom - top) / 2
         area = rec_area - tri_area
-        return  area
+        return  abs(area)
 
     def calculate_righttriangle(self, x1, y1, x2, y2, box):
         left, right, top, bottom = box
@@ -182,7 +240,7 @@ class CalculateRegion(object):
         c = [((bottom - (y1 - ((y2 - y1) / (x2 - x1) * x1))) / ((y2 - y1) / (x2 - x1))), bottom]
         ac = math.sqrt(((c[1] - a[1]) * (c[1] - a[1])) + ((c[0] - a[0]) * (c[0] - a[0])))
         ab = math.sqrt(((b[1] - a[1]) * (b[1] - a[1])) + ((b[0] - a[0]) * (b[0] - a[0])))
-        return (ac * ab / 2)
+        return abs(ac * ab / 2)
 
     def calculate_rightrectangle(self, x1, y1, x2, y2, box):
         left, right, top, bottom = box
@@ -193,7 +251,7 @@ class CalculateRegion(object):
         top_width = left - line_top
         height = bottom - top
         area = (bottom_width + top_width) * height / 2
-        return area
+        return abs(area)
 
     def calculate_rightpentagon(self, x1, y1, x2, y2, box):
         left, right, top, bottom = box
@@ -203,8 +261,27 @@ class CalculateRegion(object):
         rec_area = (bottom - top) * (right - left)
         tri_area = (right - line_top) * (line_bottom - top) / 2
         area = rec_area - tri_area
-        return area
+        return abs(area)
 
     def change_image(self, image):
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2YCrCb)
+
+        print('---------------Change Image-------------------')
+        str = "WARNINING!!!!"
+        image = cv2.putText(image, str, (0,100), cv2.FONT_HERSHEY_PLAIN, 5, (0,0,255))
+        #image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         return image
+
+    def change_video(self, image):
+        str="WARNING!!!!!"
+        self.cnt1 += 1
+        if self.Warning == False:
+            return image
+        if self.cnt1 <= 10:
+            if self.cnt1 == 10:
+                self.cnt1 = 0
+                self.Warning = False
+            print('---------------Change Image-------------------')
+            image = cv2.putText(image, str, (0, 100), cv2.FONT_HERSHEY_PLAIN, 5, (0, 0, 255))
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2XYZ)
+            return image
+
